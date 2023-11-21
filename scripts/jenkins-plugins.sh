566715327ea999fa2eb102978f75ba89fdb9de29
luisrom661 <luisrom@unicauca.edu.co>
2023-11-20 22:07:41 -0500

    Update Jenkinsfile and Jenkins plugins

--- scripts/jenkins-plugins.sh
+++ scripts/jenkins-plugins.sh
@@ -7,4 +7,9 @@ java -jar jenkins-cli.jar -s http://34.224.62.119:8080/ -auth @creds install-plu
 java -jar jenkins-cli.jar -s http://34.224.62.119:8080/ -auth @creds install-plugin sonar
 java -jar jenkins-cli.jar -s http://34.224.62.119:8080/ -auth @creds install-plugin nodejs
 java -jar jenkins-cli.jar -s http://34.224.62.119:8080/ -auth @creds install-plugin dependency-check-jenkins-plugin
+java -jar jenkins-cli.jar -s http://34.224.62.119:8080/ -auth @creds install-plugin docker-plugin
+java -jar jenkins-cli.jar -s http://34.224.62.119:8080/ -auth @creds install-plugin docker-commons
+java -jar jenkins-cli.jar -s http://34.224.62.119:8080/ -auth @creds install-plugin docker-workflow
+java -jar jenkins-cli.jar -s http://34.224.62.119:8080/ -auth @creds install-plugin docker-java-api
+java -jar jenkins-cli.jar -s http://34.224.62.119:8080/ -auth @creds install-plugin docker-build-step
 java -jar jenkins-cli.jar -s http://34.224.62.119:8080/ -auth @creds safe-restart
